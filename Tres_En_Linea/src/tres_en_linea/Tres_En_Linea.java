ad6e291ef15ce2551680de8d1401bbc0f85215e9
7BLACKHAWK <romar.726@gmail.com>
2016-02-18 13:57:52 -0600

    tres en linea inicial

---
+++ Tres_En_Linea/src/tres_en_linea/Tres_En_Linea.java
@@ -0,0 +1,26 @@
+import Vista.Menu_Principal;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author Marvin
+ */
+public class Tres_En_Linea {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.ClassNotFoundException
+     * @throws java.lang.InstantiationException
+     * @throws java.lang.IllegalAccessException
+     * @throws javax.swing.UnsupportedLookAndFeelException
+     */
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+    UIManager.setLookAndFeel( "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        Menu_Principal m1 = new Menu_Principal();
+        m1.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
+
